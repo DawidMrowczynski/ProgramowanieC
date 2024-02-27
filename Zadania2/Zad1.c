a4f36a4e27a1b10d0221a532ade51473848a376c
DawidMrowczynski <147393821+DawidMrowczynski@users.noreply.github.com>
2024-02-27 11:26:25 +0100

    Zadania 2

--- Zadania2/Zad1.c
+++ Zadania2/Zad1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main()
+{
+    int x = 3;
+    int *pointer = &x;
+    
+    printf("x: %i\n", x);
+    
+    *pointer = 5;
+    
+    printf("x changed: %i\n", x);
+
+    return 0;
+}
